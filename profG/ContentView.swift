478072862217d4fe0938fab09feb2b5f845f2045
Emmet <emmet.malone@vhi.ie>
2023-11-19 01:09:07 +0000

    Initial Commit

---
+++ profG/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  profG
+//
+//  Created by Emmet Malone on 18/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var messageString = ""
+    @State private var isButtonPressed = false  // State property to toggle visibility
+    @State private var isButtonPressedTwo = false  // State property to toggle visibility
+    
+    var body: some View {
+      
+            Text("Front-End-Dev")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(Color.red)
+                    .padding()
+                
+                HStack{
+                    Button(action: {
+                        // Action is performed when pressed
+                        messageString = "You are Awesome"
+                        isButtonPressed = false  // Toggle to true when button is pressed
+                    }) {
+                        // Buttonpressed ? value_if_true : value_if_false.
+                        Text(isButtonPressed ? "" : "Awesome")  // Ternary operator to toggle text visibility
+                    }
+                    
+                    Button(action: {
+                        // Action is performed when pressed
+                        messageString = "Greatness here"
+                        isButtonPressedTwo = false  // Toggle to true when button is pressed
+                    }) {
+                        // Butoonpressed ? value_if_true : value_if_false.
+                        Text(isButtonPressedTwo ? "" : "Great")  // Ternary operator to toggle text visibility
+                    }
+                }
+               
+                
+                
+            }
+    
+            .buttonStyle(.borderedProminent)
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+        
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ profG/profGApp.swift
@@ -0,0 +1,17 @@
+//
+//  profGApp.swift
+//  profG
+//
+//  Created by Emmet Malone on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct profGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
